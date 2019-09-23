328f2766293f77b9a3e99d69d41ba9bd96f7a57b
Lang Cheng <lang.chlorine@gmail.com>
2019-09-23 15:10:19 -0700

    Add character class

---
+++ src/character.cpp
@@ -0,0 +1,49 @@
+// Header
+#include "character.hpp"
+
+// internal
+
+// stlib
+#include <string>
+#include <algorithm>
+
+bool Character::init()
+{
+	return true;
+}
+
+// Releases all graphics resources
+void Character::destroy()
+{
+}
+
+// Called on each frame by World::update()
+void Character::update(float ms)
+{
+}
+
+void Character::draw(const mat3& projection)
+{
+}
+
+vec2 Character::get_position() const
+{
+    return vec2 {0,0};
+}
+
+void Character::move(vec2 off)
+{
+}
+
+void Character::set_rotation(float radians)
+{
+}
+
+bool Character::is_alive() const
+{
+	return true;
+}
+
+void Character::kill()
+{
+}
