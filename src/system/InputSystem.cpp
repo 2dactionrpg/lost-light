a4066d7780fefcb75f332836251adce3190961c9
Lang Cheng <lang.chlorine@gmail.com>
2019-10-03 17:07:41 -0700

    input system

--- src/system/InputSystem.cpp
+++ src/system/InputSystem.cpp
@@ -2,25 +2,32 @@
 
 void InputSystem::update(entt::registry &registry, int key, int action, int mod)
 {
-    auto view = registry.view<player, input, motion>();
+    auto view = registry.view<input>();
     for (auto entity : view)
     {
-        auto &upKeyPressed = view.get<input>(entity).upKeyPressed;
-        auto &downKeyPressed = view.get<input>(entity).downKeyPressed;
-        auto &leftKeyPressed = view.get<input>(entity).leftKeyPressed;
-        auto &rightKeyPressed = view.get<input>(entity).rightKeyPressed;
-        if (action == GLFW_RELEASE && key == GLFW_KEY_R)
-        {
-            registry.reset<motion>(entity);
-            // m_character.destroy();
-            // m_character.init();
-            // m_pebbles_emitter.destroy();
-            // m_pebbles_emitter.init();
-            // m_projectiles.clear();
-            // // m_turtles.clear();
-            // // m_fish.clear();
-            // m_water.reset_salmon_dead_time();
-            // m_current_speed = 1.f;
-        }
+        auto &upKeyPressed = view.get(entity).upKeyPressed;
+        auto &downKeyPressed = view.get(entity).downKeyPressed;
+        auto &leftKeyPressed = view.get(entity).leftKeyPressed;
+        auto &rightKeyPressed = view.get(entity).rightKeyPressed;
+
+        if (action == GLFW_PRESS && (key == GLFW_KEY_UP || key == GLFW_KEY_W))
+            upKeyPressed = true;
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_UP || key == GLFW_KEY_W))
+            upKeyPressed = false;
+
+        if (action == GLFW_PRESS && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S))
+            downKeyPressed = true;
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S))
+            downKeyPressed = false;
+
+        if (action == GLFW_PRESS && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A))
+            leftKeyPressed = true;
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A))
+            leftKeyPressed = false;
+
+        if (action == GLFW_PRESS && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D))
+            rightKeyPressed = true;
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D))
+            rightKeyPressed = false;
     }
 }
