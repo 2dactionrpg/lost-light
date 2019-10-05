e96e4119f4ae29c1d10e510b68392020e9e1569c
s9p0b <asvatukhin@gmail.com>
2019-10-04 18:29:21 -0700

    Update floor color

--- src/world.cpp
+++ src/world.cpp
@@ -386,7 +386,7 @@ void World::draw()
     // Clearing backbuffer
     glViewport(0, 0, w, h);
     glDepthRange(0.00001, 10);
-    const float clear_color[3] = { 0.3f, 0.3f, 0.8f };
+    const float clear_color[3] = { 0.16f, 0.07f, 0.05f };
     glClearColor(clear_color[0], clear_color[1], clear_color[2], 1.0);
     glClearDepth(1.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
