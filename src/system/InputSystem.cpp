f4e4b49caf14d1923630729e459de0f396018708
s9p0b <asvatukhin@gmail.com>
2019-10-18 17:45:16 -0700

    Format code

--- src/system/InputSystem.cpp
+++ src/system/InputSystem.cpp
@@ -1,84 +1,72 @@
 #include "InputSystem.hpp"
 
-void InputSystem::on_key(entt::registry &registry, int key, int action, int mod)
+void InputSystem::on_key(entt::registry& registry, int key, int action, int mod)
 {
     auto view = registry.view<motionComponent, inputKeyboard>();
-    for (auto entity : view)
-    {
-        auto &[upKeyPressed, downKeyPressed, leftKeyPressed, rightKeyPressed, resetKeyPressed] = view.get<inputKeyboard>(entity);
-        auto &[position, direction, radians, speed] = view.get<motionComponent>(entity);
+    for (auto entity : view) {
+        auto& [upKeyPressed, downKeyPressed, leftKeyPressed, rightKeyPressed, resetKeyPressed] = view.get<inputKeyboard>(entity);
+        auto& [position, direction, radians, speed] = view.get<motionComponent>(entity);
 
-        direction = {0.f, 0.f};
+        direction = { 0.f, 0.f };
 
-        if (action == GLFW_PRESS && key == GLFW_KEY_R)
-        {
-            direction = {0.f, 0.f};
+        if (action == GLFW_PRESS && key == GLFW_KEY_R) {
+            direction = { 0.f, 0.f };
             resetKeyPressed = true;
         }
-        if (action == GLFW_RELEASE && key == GLFW_KEY_R)
-        {
-            direction = {0.f, 0.f};
+        if (action == GLFW_RELEASE && key == GLFW_KEY_R) {
+            direction = { 0.f, 0.f };
             resetKeyPressed = false;
         }
 
-        if (action == GLFW_PRESS && (key == GLFW_KEY_UP || key == GLFW_KEY_W))
-        {
+        if (action == GLFW_PRESS && (key == GLFW_KEY_UP || key == GLFW_KEY_W)) {
             upKeyPressed = true;
         }
-        if (action == GLFW_RELEASE && (key == GLFW_KEY_UP || key == GLFW_KEY_W))
-        {
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_UP || key == GLFW_KEY_W)) {
             upKeyPressed = false;
         }
 
-        if (action == GLFW_PRESS && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S))
-        {
+        if (action == GLFW_PRESS && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S)) {
             downKeyPressed = true;
         }
-        if (action == GLFW_RELEASE && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S))
-        {
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_DOWN || key == GLFW_KEY_S)) {
             downKeyPressed = false;
         }
 
-        if (action == GLFW_PRESS && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A))
-        {
+        if (action == GLFW_PRESS && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A)) {
             leftKeyPressed = true;
         }
-        if (action == GLFW_RELEASE && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A))
-        {
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_LEFT || key == GLFW_KEY_A)) {
             leftKeyPressed = false;
         }
 
-        if (action == GLFW_PRESS && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D))
-        {
+        if (action == GLFW_PRESS && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D)) {
             rightKeyPressed = true;
         }
-        if (action == GLFW_RELEASE && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D))
-        {
+        if (action == GLFW_RELEASE && (key == GLFW_KEY_RIGHT || key == GLFW_KEY_D)) {
             rightKeyPressed = false;
         }
 
         if (upKeyPressed) {
-            direction = add(direction, {0.f, -1.f});
+            direction = add(direction, { 0.f, -1.f });
         }
         if (downKeyPressed) {
-            direction = add(direction, {0.f, 1.f});
-        }        
+            direction = add(direction, { 0.f, 1.f });
+        }
         if (leftKeyPressed) {
-            direction = add(direction, {-1.f, 0.f});
+            direction = add(direction, { -1.f, 0.f });
         }
         if (rightKeyPressed) {
-            direction = add(direction, {1.f, 0.f});
+            direction = add(direction, { 1.f, 0.f });
         }
     }
 }
 
-void InputSystem::on_mouse(entt::registry &registry, double xpos_new, double ypos_new)
+void InputSystem::on_mouse(entt::registry& registry, double xpos_new, double ypos_new)
 {
     auto view = registry.view<inputMouse>();
-    for (auto entity : view)
-    {
-        auto &xpos = view.get(entity).xpos;
-        auto &ypos = view.get(entity).ypos;
+    for (auto entity : view) {
+        auto& xpos = view.get(entity).xpos;
+        auto& ypos = view.get(entity).ypos;
 
         xpos = xpos_new;
         ypos = ypos_new;
--- src/system/enemyAISystem.hpp
+++ src/system/enemyAISystem.hpp
@@ -1,18 +1,17 @@
-#include <entt/entity/registry.hpp>
-#include "../components/motionComponent.hpp"
-#include "../components/enemyComponent.hpp"
-#include "../components/characterComponent.hpp"
 #include "../common.hpp"
+#include "../components/characterComponent.hpp"
+#include "../components/enemyComponent.hpp"
+#include "../components/motionComponent.hpp"
 #include "../enemy.hpp"
 #include "../projectile.hpp"
+#include <entt/entity/registry.hpp>
 
 using namespace std;
 
-struct EnemyAISystem
-{
-    void set_direction(entt::registry &registry);
-    void set_target(entt::registry &registry);
-    void set_rotation(entt::registry &registry);
-    void shoot(entt::registry &registry, float elapsed_ms, vector<Enemy> &m_enemies, vector<Projectile> &m_projectiles);
-    void destroy_dead_enemies(entt::registry &registry);
+struct EnemyAISystem {
+    void set_direction(entt::registry& registry);
+    void set_target(entt::registry& registry);
+    void set_rotation(entt::registry& registry);
+    void shoot(entt::registry& registry, float elapsed_ms, vector<Enemy>& m_enemies, vector<Projectile>& m_projectiles);
+    void destroy_dead_enemies(entt::registry& registry);
 };
