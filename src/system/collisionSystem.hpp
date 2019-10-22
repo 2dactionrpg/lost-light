d77b3467814387bf1c4253773845fa940b0872cc
Lang Cheng <lang.chlorine@gmail.com>
2019-10-21 19:57:24 -0700

    temply move collision to a different file

---
+++ src/system/collisionSystem.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "common.hpp"
+#include <vector>
+
+#include "../character.hpp"
+#include "../enemy.hpp"
+#include "../potion.hpp"
+#include "../projectile.hpp"
+#include "../shield.hpp"
+#include "enemyAISystem.hpp"
+#include "healthSystem.hpp"
+#include "inputSystem.hpp"
+#include "menuSystem.hpp"
+#include "physicsSystem.hpp"
+#include "soundSystem.hpp"
+
+using namespace std;
+
+class CollisionSystem {
+private:
+    PhysicsSystem physicsSystem;
+    EnemyAISystem enemyAI;
+    HealthSystem healthSystem;
+    MenuSystem menuSystem;
+    SoundSystem soundSystem;
+
+public:
+    // void update(entt::registry& registry, Menu& m_menu);
+    void update(entt::registry& registry, Character& m_character, Shield& m_shield, vector<Enemy>& m_enemies, vector<Projectile>& m_projectiles, Potion& m_potion, unsigned int& m_points);
+};
