631908a06107b80314de6844680eb414a6dd7eb8
LangLangSAMA <lang.chlorine@gmail.com>
2019-11-13 16:51:18 -0800

    set initial condition for enemy

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -68,6 +68,10 @@ bool Enemy::init(int id)
     enemy_id = id;
     has_path = false;
 
+    motion.position = {0.f, 0.f};
+    motion.radians = 0.f;
+    physics.scale = {0.f, 0.f};
+
     // motion.radians = M_PI + M_PI / 2;
 
     return trig.init(id) && path.init(id);
@@ -85,7 +89,57 @@ void Enemy::destroy()
     glDeleteShader(effect.program);
 }
 
-void Enemy::draw(const mat3& projection)
+void Enemy::draw(const mat3 &projection)
+{
+    // Transformation code, see Rendering and Transformation in the template specification for more info
+    // Incrementally updates transformation matrix, thus ORDER IS IMPORTANT
+    transform.begin();
+    transform.translate(motion.position);
+    transform.rotate(motion.radians);
+    transform.scale(physics.scale);
+    transform.end();
+
+    // Setting shaders
+    glUseProgram(effect.program);
+
+    // Enabling alpha channel for textures
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_DEPTH_TEST);
+
+    // Getting uniform locations for glUniform* calls
+    GLint transform_uloc = glGetUniformLocation(effect.program, "transform");
+    GLint color_uloc = glGetUniformLocation(effect.program, "fcolor");
+    GLint projection_uloc = glGetUniformLocation(effect.program, "projection");
+
+    // Setting vertices and indices
+    glBindVertexArray(mesh.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ibo);
+
+    // Input data location as in the vertex buffer
+    GLint in_position_loc = glGetAttribLocation(effect.program, "in_position");
+    GLint in_texcoord_loc = glGetAttribLocation(effect.program, "in_texcoord");
+    glEnableVertexAttribArray(in_position_loc);
+    glEnableVertexAttribArray(in_texcoord_loc);
+    glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *)0);
+    glVertexAttribPointer(in_texcoord_loc, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *)sizeof(vec3));
+
+    // Enabling and binding texture to slot 0
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, enemy_texture.id);
+
+    // Setting uniform values to the currently bound program
+    glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float *)&transform.out);
+    float color[] = {1.f, 1.f, 1.f};
+    glUniform3fv(color_uloc, 1, color);
+    glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float *)&projection);
+
+    // Drawing!
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
+}
+
+void Enemy::draw(const mat3 &projection, bool debug)
 {
     // Transformation code, see Rendering and Transformation in the template specification for more info
     // Incrementally updates transformation matrix, thus ORDER IS IMPORTANT
@@ -134,61 +188,10 @@ void Enemy::draw(const mat3& projection)
     // Drawing!
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
 
-}
-
-void Enemy::draw(const mat3& projection, bool debug)
-{
-    // Transformation code, see Rendering and Transformation in the template specification for more info
-    // Incrementally updates transformation matrix, thus ORDER IS IMPORTANT
-    transform.begin();
-    transform.translate(motion.position);
-    transform.rotate(motion.radians);
-    transform.scale(physics.scale);
-    transform.end();
-
-    // Setting shaders
-    glUseProgram(effect.program);
-
-    // Enabling alpha channel for textures
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glDisable(GL_DEPTH_TEST);
-
-    // Getting uniform locations for glUniform* calls
-    GLint transform_uloc = glGetUniformLocation(effect.program, "transform");
-    GLint color_uloc = glGetUniformLocation(effect.program, "fcolor");
-    GLint projection_uloc = glGetUniformLocation(effect.program, "projection");
-
-    // Setting vertices and indices
-    glBindVertexArray(mesh.vao);
-    glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ibo);
-
-    // Input data location as in the vertex buffer
-    GLint in_position_loc = glGetAttribLocation(effect.program, "in_position");
-    GLint in_texcoord_loc = glGetAttribLocation(effect.program, "in_texcoord");
-    glEnableVertexAttribArray(in_position_loc);
-    glEnableVertexAttribArray(in_texcoord_loc);
-    glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void*)0);
-    glVertexAttribPointer(in_texcoord_loc, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void*)sizeof(vec3));
-
-    // Enabling and binding texture to slot 0
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, enemy_texture.id);
-
-    // Setting uniform values to the currently bound program
-    glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float*)&transform.out);
-    float color[] = { 1.f, 1.f, 1.f };
-    glUniform3fv(color_uloc, 1, color);
-    glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float*)&projection);
-
-    // Drawing!
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
-
-    if(debug)
-    {  
+    if (debug)
+    {
         trig.draw(projection);
-        if(has_path)
+        if (has_path)
         {
             path.draw(projection);
         }
@@ -271,15 +274,15 @@ bool Enemy::on_sight(vec2 target)
     return trig.is_inside(target);
 }
 
-bool Enemy::set_line(vec2 target, std::vector<Enemy>& m_enemies)
+bool Enemy::set_line(vec2 target, std::vector<Enemy> &m_enemies)
 {
     has_path = true;
     std::vector<Dot> dots = path.form(get_face_position(), get_bounding_box(), target);
-    for(auto d : dots)
+    for (auto d : dots)
     {
-        for(auto enemy : m_enemies)
+        for (auto enemy : m_enemies)
         {
-            if(enemy_id != enemy.get_id() && enemy.collides_with_point(d))
+            if (enemy_id != enemy.get_id() && enemy.collides_with_point(d))
             {
                 path.blocked();
                 return false;
@@ -287,7 +290,6 @@ bool Enemy::set_line(vec2 target, std::vector<Enemy>& m_enemies)
         }
     }
     return true;
-
 }
 void Enemy::unset_line()
 {
